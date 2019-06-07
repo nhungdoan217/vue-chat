6820f1fd395e7dd361b22abae6971ab77e2b0523
Saana Tauriainen <saana.tauriainen@vaimo.com>
2019-06-07 18:17:09 +0300

    Added AddUser component

---
+++ frontend/src/components/AddUser.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <br>
+        <p>To join the chat submit your username</p>
+        <input type="text" v-model="nameClone" placeholder="Add username">
+        <br>
+        <div v-if="nameClone">
+            <b-button v-on:click="getUserName(nameClone)">Submit</b-button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AddUser',
+  props: {
+    userName: String,
+    getUserName: Function
+  },
+  data: function () {
+    return {
+        nameClone: this.userName
+    }
+  }
+}
+</script>

c1c650cb563df815905ec29059cc14569a631d7c
trinh.nam <trinh.nam@vaimo.com>
2019-06-07 15:50:33 +0300

    data to server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', (socket) => { console.log(socket, 'Connected') });
+io.on('connection', (socket) => { 
+    console.log("connected: " + socket.id);
+
+    socket.emit('namct', (data) => {
+        console.log(data, "123")
+
+        socket.on('addEvent', {data: 'test123'})
+    })
+
+    socket.on('get', function (data) {
+        console.log(data)
+    })
+
+    socket.on('addEvent', function (data) {
+        console.log(data, 'tesdasd')
+    })
+
+ });
 server.listen(3000, () => {
     console.log('Listening to port 3000')
 });
--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,38 +1,50 @@
 <template>
-    <div class="chat-service">
-        <b-badge>{{headerMsg}}</b-badge>
-    </div>
+  <div class="chat-service">
+    <b-badge>{{headerMsg}}</b-badge>
+    <button v-on:click="add">TEST connect</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Chat',
-  props: {
-    headerMsg: String
-  },
+    name: 'Chat',
+    props: {
+        headerMsg: String,
+    },
     methods: {
-            add() {
-          		// Emit the server side
-          		this.$socket.emit("add", { a: 5, b: 3 });
-            },
- 
-            get() {
-          		this.$socket.emit("get", { id: 12 }, (response) => {
-                    console.log();
-                });
-            }
+        disconnect: function() {
+            this.logs = []
         },
+        add() {
+            console.log('213')
+            // Emit the server side
+            this.$socket.emit('addEvent', { a: 5, b: 3 }, response => {
+                console.log(response, 'hsadhausad')
+            })
+        },
+
+        get() {
+            this.$socket.emit('get', { id: 12 }, response => {
+                console.log(response)
+            })
+        },
+        namct() {
+            console.log('namct event emit')
+
+            this.methods.add()
+        },
+    },
     sockets: {
         connect: function() {
-                console.log('Connected')
-            }
-        }    
+            console.log('Connected')
+        },
+    },
 }
 </script>
 
 
 <style scoped>
 .chat-service {
-  color: red;
+    color: red;
 }
 </style>

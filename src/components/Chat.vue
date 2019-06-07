3bc326e3fa5a8387108f1bb3a348b44ebad0f904
Nhung Doan <nhungdoan@Nhungs-MacBook-Pro.local>
2019-06-07 13:57:20 +0300

    Add bootstrap and test render bootstrap component

---
+++ src/components/Chat.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="chat-service">
+        <b-badge>{{headerMsg}}</b-badge>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Chat',
+  props: {
+    headerMsg: String
+  }
+}
+</script>
+
+
+<style scoped>
+.chat-service {
+  color: red;
+}
+</style>

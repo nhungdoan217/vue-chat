88ae97f8bfb6520a24102441855a54ee1ae75e58
Nhung Doan <nhungdoan@Nhungs-MacBook-Pro.local>
2019-06-07 18:52:54 +0300

    Render chat

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,33 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const room = {default: {
+    id: 123,
+    name: 'Vaimo',
+    onlineUsers: 0,
+    onlineUserIds: []
+}};
 io.on('connection', (socket) => { 
     console.log("connected: " + socket.id);
 
-    socket.emit('namct', (data) => {
-        console.log(data, "123")
+    socket.join(room.default);
 
-        socket.on('addEvent', {data: 'test123'})
+    socket.on('joinRoom', (user, ack) => {
+        socket.join(room.default.id);
+        if (!room.default.onlineUserIds.includes(user.id)) {
+            room.default.onlineUserIds.push(socket.id);    
+            room.default.onlineUsers += 1;
+            ack({room: room, user: user})
+        }
+    });
+
+    socket.on('disconnect', () => {
+        const index = room.default.onlineUserIds.indexOf(socket.id);
+        if (index != -1) {
+            room.default.onlineUserIds.shift(index);
+        }
+        console.log('Disconnected', room.default.onlineUserIds);
     })
-
-    socket.on('get', function (data) {
-        console.log(data)
-    })
-
-    socket.on('addEvent', function (data) {
-        console.log(data, 'tesdasd')
-    })
-
  });
 server.listen(3000, () => {
     console.log('Listening to port 3000')
--- frontend/src/components/Chat.vue
+++ frontend/src/components/Chat.vue
@@ -1,38 +1,72 @@
 <template>
   <div class="chat-service">
     <b-badge>{{headerMsg}}</b-badge>
-    <button v-on:click="add">TEST connect</button>
+    <button v-on:click="addUser" v-if="!isUserExists">Test connect</button>
+    <div v-if="isUserExists">
+        <button v-on:click="disconnect">Disconnect</button>
+    </div>
+    <div v-if="room.default">
+        <div v-for="onlineUserId in onlineUserIds">
+            {{user[onlineUserId]}}
+            {{user}}
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
 export default {
     name: 'Chat',
     props: {
         headerMsg: String,
     },
+    data() {
+        return {
+            user: {},
+            room: {},
+            onlineUserIds: []
+        }
+    },
     methods: {
         disconnect: function() {
             this.logs = []
         },
-        add() {
-            console.log('213')
-            // Emit the server side
-            this.$socket.emit('addEvent', { a: 5, b: 3 }, response => {
-                console.log(response, 'hsadhausad')
+        getSocket() {
+            return this.$socket;
+        },
+        addUser() {
+            this.getSocket().emit('joinRoom', {name: 'Nhung', id: this.$socket.id}, response => {
+                this.room = response.room;
+                this.user[response.user.id] = response.user;
+                this.onlineUserIds = response.room.default.onlineUserIds
             })
         },
-
+        getUser() {
+            this.getSocket().on('joinRoomResp', (res) => {
+                console.log(res)
+            })
+        },
         get() {
             this.$socket.emit('get', { id: 12 }, response => {
                 console.log(response)
             })
         },
-        namct() {
-            console.log('namct event emit')
-
-            this.methods.add()
-        },
+        disconnect() {
+            this.$socket.emit('disconnect', () => {
+                this.user = {};
+                this.room = {};
+                this.onlineUserIds = {};
+            })
+        }
+    },
+    computed: {
+        isUserExists: function() {
+            if (!_.isEmpty(this.user) && !_.isEmpty(this.room)) {
+                return this.room.default.onlineUserIds.includes(this.user.id);
+            }
+            return false;
+        }
     },
     sockets: {
         connect: function() {
